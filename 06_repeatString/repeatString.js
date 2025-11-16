7a5aa6e24e4034797ee68deffb4287d2ada8a959
Vrinz <19779901+vrinz@users.noreply.github.com>
2025-11-16 16:32:07 +0800

    completed js exercise 6 - repeat strings

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function (word, loop) {
+	if (loop < 0) {
+		return "ERROR";
+	}
+	let string = "";
+    
+	for (let i = 0; i < loop; i++) {
+		string += word;
+	}
+	return string;
 };
 
 // Do not edit below this line
